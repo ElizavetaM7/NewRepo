337d40a371efe0c426ae167b6ece922d89a51cf3
Elizaveta.Mamzeleva@urfu.me <Elizaveta.Mamzeleva@urfu.me>
2023-06-15 17:13:11 +0500

    Метод LINQ

---
+++ MethodLINQ/MethodLINQ/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodLINQ
+{
+    internal class Program
+    {
+        public static int[] SortArray(int[] arr)
+        {
+            return arr.Where(x => x > 0)
+                      .OrderByDescending(x => x.ToString().Length)
+                      .ThenBy(x => x)
+                      .ToArray();
+        }
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -5,18,65,-67};
+            int[] sortedArr = SortArray(arr);
+            Console.WriteLine(string.Join(", ", sortedArr));
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ MethodLINQ/MethodLINQ/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("MethodLINQ")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MethodLINQ")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("80f33e02-516f-4b3c-8f0a-842ab5e2a03a")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
